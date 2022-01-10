9f67a0f9189080090df6c8c85f645bb2482bbe4c
RicardoEspinosaLoera <31373900+RicardoEspinosaLoera@users.noreply.github.com>
2022-01-10 05:03:45 -0600

    Update gen_data.py

--- dataset/gen_data.py
+++ dataset/gen_data.py
@@ -39,7 +39,7 @@ import dataset_loader
 import numpy as np
 import scipy.misc
 import tensorflow as tf
-import imageio
+import cv2
 
 gfile = tf.io.gfile
 FLAGS = flags.FLAGS
@@ -166,7 +166,7 @@ def _gen_example(i, all_examples):
     gfile.makedirs(save_dir)
   img_filepath = os.path.join(save_dir, '%s.jpg' % example['file_name'])
   #scipy.misc.imsave(img_filepath, image_seq_stack.astype(np.uint8))
-  imageio.imwrite(img_filepath, image_seq_stack.astype(np.uint8))
+  cv2.imwrite(img_filepath, image_seq_stack.astype(np.uint8))
   cam_filepath = os.path.join(save_dir, '%s_cam.txt' % example['file_name'])
   example['cam'] = '%f,0.,%f,0.,%f,%f,0.,0.,1.' % (fx, cx, fy, cy)
   with open(cam_filepath, 'w') as cam_f:
