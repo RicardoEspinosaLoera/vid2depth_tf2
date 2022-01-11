ecff89b3a9e7105434f93a20134c6304d2d2a55f
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-01-11 09:01:42 -0600

    debug

--- reader.py
+++ reader.py
@@ -54,7 +54,7 @@ class DataReader(object):
         seed = random.randint(0, 2**31 - 1)
         self.file_lists = self.compile_file_list(self.data_dir, 'train')
         f.write("file_lists")
-        f.write(str(self.file_lists.shape))
+        f.write(str(len(self.file_lists)))
         image_paths_queue = tf.compat.v1.train.string_input_producer(self.file_lists['image_file_list'], seed=seed, shuffle=True)
         #image_paths_queue = tf.data.TextLineDataset(self.file_lists['image_file_list'])
         cam_paths_queue = tf.compat.v1.train.string_input_producer(self.file_lists['cam_file_list'], seed=seed, shuffle=True)
