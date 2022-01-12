511a62d1c89d2bed73dd2594b776b553fbedd10e
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-01-12 04:37:11 -0600

    tes debug

--- test_read.py
+++ test_read.py
@@ -39,44 +39,14 @@ gfile = tf.io.gfile
 
 NUM_SCALES = 4
 
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-import shutil
-import os
-
-if not os.path.exists('example'):
-    os.mkdir('example')
-
-batch_sz = 10; epochs = 2; buffer_size = 30; samples = 0
-for i in range(50):
-    _x = np.random.randint(0, 256, (10, 10, 3), np.uint8)
-    plt.imsave("example/image_{}.jpg".format(i), _x)
-images = tf.io.match_filenames_once('example/*.jpg')
 seed = random.randint(0, 2**31 - 1)
-image_paths_queue = tf.compat.v1.train.string_input_producer(images, seed=seed, shuffle=True)
-#fname_q = tf.train.string_input_producer(images,epochs, True);
-reader = tf.compat.v1.WholeFileReader()
-_, value = reader.read(image_paths_queue)
-img = tf.image.decode_image(value)
-img_batch = tf.train.batch([img], batch_sz, shapes=([10, 10, 3]))
-with tf.Session() as sess:
-    sess.run([tf.global_variables_initializer(),
-        tf.local_variables_initializer()])
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
-    for _ in range(epochs):
-        try:
-            while not coord.should_stop():
-                sess.run(img_batch)
-                samples += batch_sz
-                print(samples, "samples have been seen")
-        except tf.errors.OutOfRangeError:
-            print('Done training -- epoch limit reached')
-        finally:
-            coord.request_stop()
-    coord.join(threads)
-
+file_lists = compile_file_list("/workspace/vid2depth/vid2depth_tf2/data", 'train')
+image_paths_queue = tf.compat.v1.train.string_input_producer(file_lists, seed=seed, shuffle=True)
+cam_paths_queue = tf.compat.v1.train.string_input_producer(self.file_lists['cam_file_list'], seed=seed, shuffle=True)
+#cam_paths_queue = tf.data.TextLineDataset(self.file_lists['cam_file_list'])
+img_reader = tf.compat.v1.WholeFileReader()
+_, image_contents = img_reader.read(image_paths_queue)
+image_seq = tf.image.decode_image(image_contents)
 """
 reader = reader.DataReader("/workspace/vid2depth/vid2depth_tf2/data", 4,128, 416, 3, NUM_SCALES)
 image_stack, intrinsic_mat, intrinsic_mat_inv,image_contents = reader.read_data()
@@ -112,3 +82,29 @@ with tf.Session() as sess:
         pass
     print('Done training -- epoch limit reached')
 """
+
+def compile_file_list(self, data_dir, split, load_pose=False):
+    with gfile.GFile(os.path.join(data_dir, '%s.txt' % split), 'r') as f:
+      frames = f.readlines()
+      subfolders = [x.split(' ')[0] for x in frames]
+      frame_ids = [x.split(' ')[1][:-1] for x in frames]
+      image_file_list = [
+          os.path.join(data_dir, subfolders[i], frame_ids[i] + '.jpg')
+          for i in range(len(frames))
+      ]
+      cam_file_list = [
+          os.path.join(data_dir, subfolders[i], frame_ids[i] + '_cam.txt')
+          for i in range(len(frames))
+      ]    
+      
+      file_lists = {}
+      file_lists['image_file_list'] = image_file_list
+      file_lists['cam_file_list'] = cam_file_list
+      if load_pose:
+        pose_file_list = [
+            os.path.join(data_dir, subfolders[i], frame_ids[i] + '_pose.txt')
+            for i in range(len(frames))
+        ]
+        file_lists['pose_file_list'] = pose_file_list
+      self.steps_per_epoch = len(image_file_list) // self.batch_size
+    return file_lists
