5aad8447bc1bebab9ea71e0a165122f8a3538b80
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-01-12 08:57:16 -0600

    debug

--- test_read.py
+++ test_read.py
@@ -76,9 +76,9 @@ def unpack_images(image_seq):
         image_seq[:, i * 416:(i + 1) * 416, :]
         for i in range(3)
     ]
-    print("unpack_images "+str(len(image_list[0])))
-    print("unpack_images "+str(len(image_list[1])))
-    print("unpack_images "+str(len(image_list[2])))
+    print("unpack_images "+str(image_list[0].shape))
+    print("unpack_images "+str(image_list[1].shape))
+    print("unpack_images "+str(image_list[2].shape))
     image_stack = tf.concat(image_list, axis=2)
     image_stack.set_shape(
         [128, 416, 3 * 3])
