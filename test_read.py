f466f9d9f486ef74829604891f158b870e5f52b6
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-01-12 04:58:18 -0600

    file_lists

--- test_read.py
+++ test_read.py
@@ -71,6 +71,7 @@ def compile_file_list(data_dir, split, load_pose=False):
 
 seed = random.randint(0, 2**31 - 1)
 file_lists = compile_file_list("/workspace/vid2depth/vid2depth_tf2/data", 'train')
+print(len(file_lists))
 image_paths_queue = tf.compat.v1.train.string_input_producer(file_lists['cam_file_list'], seed=seed, shuffle=True)
 #cam_paths_queue = tf.compat.v1.train.string_input_producer(file_lists['cam_file_list'], seed=seed, shuffle=True)
 #cam_paths_queue = tf.data.TextLineDataset(self.file_lists['cam_file_list'])
