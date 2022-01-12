cc6c52abb4f56fc806af1d0e4aad6305bc2715bc
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-01-12 04:02:51 -0600

    debug

--- test_read.py
+++ test_read.py
@@ -28,15 +28,52 @@ import reader
 import tensorflow as tf
 import util
 import tf_slim as slim
+import numpy as np
+import matplotlib.pyplot as plt
+import shutil
+import os
 
 tf.compat.v1.disable_eager_execution()
 gfile = tf.io.gfile
 
 NUM_SCALES = 4
 
+"""
 reader = reader.DataReader("/workspace/vid2depth/vid2depth_tf2/data", 4,128, 416, 3, NUM_SCALES)
 image_stack, intrinsic_mat, intrinsic_mat_inv,image_contents = reader.read_data()
 print(image_stack.shape)
 print(intrinsic_mat.shape)
 print(str(image_contents))
+"""
+
+if not os.path.exists('example'):
+    shutil.rmTree('example')
+    os.mkdir('example')
+
+batch_sz = 10; epochs = 2; buffer_size = 30; samples = 0
+for i in range(50):
+    _x = np.random.randint(0, 256, (10, 10, 3), np.uint8)
+    plt.imsave("example/image_{}.jpg".format(i), _x)
+images = tf.train.match_filenames_once('example/*.jpg')
+fname_q = tf.train.string_input_producer(images,epochs, True)
+reader = tf.WholeFileReader()
+_, value = reader.read(fname_q)
+img = tf.image.decode_image(value)
+img_batch = tf.train.batch([img], batch_sz, shapes=([10, 10, 3]))
+with tf.Session() as sess:
+    sess.run([tf.global_variables_initializer(),
+        tf.local_variables_initializer()])
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    for _ in range(epochs):
+        try:
+            while not coord.should_stop():
+                sess.run(img_batch)
+                samples += batch_sz
+                print(samples, "samples have been seen")
+        except tf.errors.OutOfRangeError:
+            print('Done training -- epoch limit reached')
+        finally:
+            coord.request_stop()
+    coord.join(threads)
 
