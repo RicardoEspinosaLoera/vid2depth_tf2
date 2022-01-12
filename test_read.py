6a31727893d03bbc04d139a7555e6590f1f4bcdd
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-01-12 09:10:04 -0600

    debug

--- test_read.py
+++ test_read.py
@@ -90,8 +90,8 @@ seed = random.randint(0, 2**31 - 1)
 file_lists = compile_file_list("/workspace/vid2depth/vid2depth_tf2/data", 'train')
 for a in file_lists:
   print(a)
-  img = cv2.imread(a)
-  print(img.shape)
+  #img = cv2.imread(a)
+  #print(img.shape)
 """
 image_paths_queue = tf.compat.v1.train.string_input_producer(file_lists['image_file_list'], seed=seed, shuffle=True)
 #cam_paths_queue = tf.data.TextLineDataset(file_lists['cam_file_list'])
