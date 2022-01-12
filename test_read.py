612d4f1e5b138857bcf646ddea35368342a66912
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2022-01-12 09:09:04 -0600

    debug

--- test_read.py
+++ test_read.py
@@ -89,6 +89,7 @@ def unpack_images(image_seq):
 seed = random.randint(0, 2**31 - 1)
 file_lists = compile_file_list("/workspace/vid2depth/vid2depth_tf2/data", 'train')
 for a in file_lists:
+  print(a)
   img = cv2.imread(a)
   print(img.shape)
 """
